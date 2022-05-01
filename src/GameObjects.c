c0792c8ee555a33ead9659c0605f8d5b124b3468
mazarona <mazenfahem@gmail.com>
2022-05-01 06:02:36 +0200

    small fix

--- src/GameObjects.c
+++ src/GameObjects.c
@@ -1,7 +1,16 @@
 #include "../include/GameObjectsPrivate.h"
+#include "../include/linked_list.h"
+#include <stdlib.h>
+
+/* Keep track of created game objects in a linked list*/
+List squares;
+static void destroySquares(void *data){free((Square *) data);}
 
 void createSquare(float x, float y, int size, float xSpeed, float ySpeed,
         int red, int green, int blue, int alpha, void(*update)(struct Square*))
 {
+    list_init(&squares, destroySquares);
+    
 
 }
+
